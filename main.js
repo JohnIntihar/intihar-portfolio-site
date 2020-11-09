d0c007a59f52a5d8cda5cca3f75c38b92e99fa14
John Intihar <intihar.john@gmail.com>
2020-11-09 15:37:58 -0500

    Finalized organization of elements and page details

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
 
-                                                          /* Start Scroll Code */
-
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
-
+// ********** Page Scroll ************
 
 function startTopofScreen() {
   window.scroll(1,1); 
 }
 window.onload = startTopofScreen;
 
-// ********** set date ************
-// select span
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
-// ********** close links ************
 const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
@@ -25,8 +16,6 @@ const links = document.querySelector(".links");
 navToggle.addEventListener("click", function () {
   linksContainer.classList.toggle("show-links");
 });
-
-// ********** fixed navbar ************
 
 const navbar = document.getElementById("nav");
 const topLink = document.querySelector(".top-link");
@@ -39,23 +28,20 @@ window.addEventListener("scroll", function () {
   } else {
     navbar.classList.remove("fixed-nav");
   }
-  // setup back to top link
-
+  
   if (scrollHeight > 500) {
-    topLink.classList.add("show-link");
+    topLink.classList.add("show-top-link");
   }
   else {
-    topLink.classList.remove("show-link");
+    topLink.classList.remove("show-top-link");
   }
 });
 
-// ********** smooth scroll ************
-// select links
 const scrollLinks = document.querySelectorAll(".scroll-link");
 scrollLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
     e.preventDefault();
-    // navigate to specific spot
+
     const id = e.currentTarget.getAttribute("href").slice(1);
     const element = document.getElementById(id);
 
@@ -75,18 +61,12 @@ scrollLinks.forEach((link) => {
       left: 0,
       top: position,
     });
-    // close
+
     linksContainer.style.height = 0;
   });
 });
-                                                          /* End Scroll Code */
 
 // ********** Resume Modal ************
-
-// select modal-btn,modal-overlay,close-btn
-// listen for click events on modal-btn and close-btn
-// when user clicks modal-btn add .open-modal to modal-overlay
-// when user clicks close-btn remove .open-modal from modal-overlay
 
 const modalOverlay = document.querySelector('.modal-overlay');
 const openModal = document.querySelector('.modal-btn');
@@ -120,44 +100,48 @@ about.addEventListener('click', function(e){
         element.classList.add('active');
     }
 });
+
 // ********** Endorsements ************
 const reviews = [
   {
     id: 1,
-    name: "susan smith",
-    job: "web developer",
+    name: "Lukas Van Houten",
+    job: "Senior Full-Stack Software Developer",
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      "https://source.unsplash.com/random",
     text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+      "John dec, yo. Straight bitchin'.",
   },
+
   {
     id: 2,
-    name: "anna johnson",
-    job: "web designer",
+    name: "Michael P",
+    job: "Accountant",
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      "https://source.unsplash.com/random",
     text:
-      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+      "John good work",
   },
-  {
-    id: 3,
-    name: "peter jones",
-    job: "intern",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-    text:
-      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-  },
-  {
-    id: 4,
-    name: "bill anderson",
-    job: "the boss",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-    text:
-      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-  },
+
+  // {
+  //   id: 3,
+  //   name: "",
+  //   job: "",
+  //   img:
+  //     "",
+  //   text:
+  //     "",
+  // },
+
+  // {
+  //   id: 4,
+  //   name: "",
+  //   job: "",
+  //   img:
+  //     "",
+  //   text:
+  //     "",
+  // },
 ];
 
 const img = document.getElementById('person-img');
@@ -171,7 +155,6 @@ const randomBtn = document.querySelector('.random-btn');
 
 let currentItem = 0;
 
-// DOMContentLoaded is just the event where the initial HTML has been completely loaded and parsed
 window.addEventListener('DOMContentLoaded',function(){
   showPerson(currentItem);
 });
